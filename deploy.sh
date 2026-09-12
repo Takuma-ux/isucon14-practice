f42f47ec4b79b84adffdd2e5b0ba8648c9267263
keita <fun.keitakuma4@icloud.com>
2026-09-12 09:58:10 +0900

    deploy.sh change for python

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,13 @@ set -e
 cd /home/isucon/isucon14-practice
 git pull origin feature/keita_test
 
-sudo systemctl stop isuride-go
+sudo systemctl stop isuride-python
 
-cp -a go/*.go go/go.mod go/go.sum /home/isucon/webapp/go/
+cp -a python/. /home/isucon/webapp/python/
 
-cd /home/isucon/webapp/go
-go build -o isuride
+cd /home/isucon/webapp/python
+# 依存関係を足したらコメントを外す
+# ~/.local/bin/uv sync
 
-sudo systemctl start isuride-go
+sudo systemctl start isuride-python
 echo "deploy done"
